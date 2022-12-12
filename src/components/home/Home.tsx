96278697158ec15f8f89830c5dd61fe5e632aa43
Ben Matela <bmatela@afrogeek.co.za>
2022-12-12 04:54:46 +0200

    home base done

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -14,7 +14,7 @@ export const Home = () => {
       <div className="md:grid md:grid-cols-2">
         <div>
         <img data-testid="bgImg" className="md:hidden sm:flex" src="https://biztweak.org.za/public/new/images/hero-main.png" height={500} width={500} />
-          <h1 data-testid="homeHeaderText" className="ml-9 tracking-wide m-auto pt-44">
+          <h1 data-testid="homeHeaderText" className="ml-10 tracking-wide m-auto pt-36">
             Take a <span>FREE</span>. BizTweak Assessment.
           </h1>
           <p data-testid="homeSubHeaderText" className="text-gray-800 ml-9 mx-auto text-1xl mt-2">
@@ -24,7 +24,7 @@ export const Home = () => {
             id={"click"}
             data-testid={"addToCartBtn"}
             label={constants.takeFreeAssessment}
-            className="hover:bg-[#2becf6] hover:border-[#00a9b1] pt-1 ml-3 capitalize shadow-lg border-[#00c2cb] bg-[#00c2cb] text-white mb-4 mt-2 text-center rounded-sm"
+            className="hover:bg-[#2becf6] hover:border-[#00a9b1] pt-1 ml-8 capitalize shadow-lg border-[#00c2cb] bg-[#00c2cb] text-white mb-4 mt-2 text-center rounded-sm"
             onClick={(event: any) => handleClick(event)}
             disabled={false}
           />
