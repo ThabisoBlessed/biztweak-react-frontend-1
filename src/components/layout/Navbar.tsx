b16bbf6c3451c0f682d08873e2230d173bc97cbe
Ben Matela <bmatela@afrogeek.co.za>
2022-12-14 05:25:05 +0200

    init nav page

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import LogoImg from "../../logo.png";
+import LogoImg from "../../images/logo.png";
 import { useNavigate } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import * as constants from "../../constants";
@@ -16,63 +15,89 @@ export const Navbar = () => {
 
   return (
     <div className="border-b" data-testid="navbar">
-      <div className="p-0 flex justify-between items-center w-[100%] mx-auto">
-        <h1 className="w-full text-2xl font-bold">
-          <Link to="/" className="p-3 flex">
-            <img
-              data-testid="logoImg"
-              alt="logo"
-              src={LogoImg}
-              height={150}
-              width={150}
-            ></img>
-          </Link>
-        </h1>
-        <ul className="hidden md:flex">
-        <li className="p-4 pr-15 text-[]" data-testid="homeText">
-            <Link to="/lms" className="p-2 flex capitalize hover:text-[#00c2cb]">
-              {constants.lms.toUpperCase()}
-            </Link>
-          </li>
-
-          <li className="p-4 pr-15 text-[]" data-testid="homeText">
-            <Link to="/" className="p-2 flex capitalize hover:text-[#00c2cb]">
-              {constants.home}
-            </Link>
-          </li>
-        </ul>
-
-        <div
-          id="mobileOpenCloseBtns"
-          onClick={handleNav}
-          className="block md:hidden pr-3"
-        >
-          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      <nav className="navbar navbar-expand-md border-bottom navbar-light bg-white">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="javascript:void(0)">
+            <img src={LogoImg}  height={160}
+              width={160} alt="logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#mynavbar"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <form className="d-flex">
+            <div className="input-group">
+              <div className="input-group-text bg-light border-0">
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </div>
+              <input
+                className="bg-light border-0 form-control me-2"
+                type="text"
+                placeholder="Search here"
+              />
+            </div>
+          </form>
+          <div className="collapse navbar-collapse" id="mynavbar">
+            <ul className="navbar-nav align-items-center ms-auto">
+              <li className="nav-item me-2">
+                <select className="form-select form-select-sm d-inline" id="">
+                  <option value="">English</option>
+                </select>
+              </li>
+              <li className="nav-item me-2">
+                <a className="nav-link" href="javascript:void(0)">
+                  <i className="fa-lg fa-solid fa-house"></i>
+                </a>
+              </li>
+              <li className="nav-item me-2">
+                <a className="nav-link" href="javascript:void(0)">
+                  <i className="fa-lg fa-solid fa-bell"></i>
+                </a>
+              </li>
+              <li className="nav-item dropdown no-arrow">
+                <a
+                  className="nav-link dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  href="javascript:void(0)"
+                >
+                  <span className="d-flex flex-nowrap align-items-center">
+                    <div className="me-2">
+                      <img
+                        src="../images/avatar.png"
+                        width="36px"
+                        className="img-fluid rounded-circle"
+                        alt=""
+                      />
+                    </div>
+                    <div>
+                      <h6 className="mb-0">Alfred Sinaga</h6>
+                      <p className="mb-0 small text-muted">alfreds@email.com</p>
+                    </div>
+                  </span>
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link to="/auth/edit-profile">
+                      <a className="dropdown-item" href="#">
+                        Edit Profile
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
-
-        <div
-          id="mobileMenu"
-          className={
-            nav
-              ? "fixed left-0 top-0 w-[60%] h-full border bg-[#00c2cb] text-white rounded-lg ease-in-out duration-500 shadow-lg"
-              : "fixed left-[-100%]"
-          }
-        >
-          <ul className="pt-18 uppercase">
-            <li className="p-2 border-b-2" onClick={handleNav}>
-              <Link to="/" className="p-4 flex mx-auto">
-                {constants.home}
-              </Link>
-            </li>
-
-            <li className="p-2 border-white" onClick={handleNav}>
-              <Link to="/" className="flex text-white">
-                {constants.home}
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
+      </nav>
     </div>
   );
 };
