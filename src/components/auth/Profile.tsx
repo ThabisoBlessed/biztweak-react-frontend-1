9d95d39f602ef065dfff12fe882dbfff943479ef
Ben Matela <bmatela@afrogeek.co.za>
2022-12-16 09:56:59 +0200

    init modal

--- src/components/auth/Profile.tsx
+++ src/components/auth/Profile.tsx
@@ -45,31 +45,117 @@ export const Profile = () => {
             aria-labelledby="addCompanyLabel"
             aria-hidden="true"
           >
-            <div className="modal-dialog">
+            <div className="modal-dialog modal-dialog-centered">
               <div className="modal-content">
                 <div className="modal-header">
-                  <h5 className="modal-title" id="addCompanyLabel">
-                    Modal title
-                  </h5>
                   <button
                     type="button"
                     className="btn-close"
                     data-bs-dismiss="modal"
                     aria-label="Close"
-                  ></button>
+                  ><i className="fa fa-close"></i></button>
                 </div>
-                <div className="modal-body">...</div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    data-bs-dismiss="modal"
+                <div className="modal-body">
+                  <div className="text-center">
+                    <img
+                      src="https://biztweak.org.za/public/new/images/add-company.png"
+                      alt=""
+                      className="col-md-6 img-fluid"
+                    />
+                  </div>
+                  <h5>Add new company</h5>
+                  <form
+                    action="https://biztweak.org.za/public/102/save-company-new"
+                    method="POST"
                   >
-                    Close
-                  </button>
-                  <button type="button" className="btn btn-primary">
-                    Save changes
-                  </button>
+                    <input type="hidden" />{" "}
+                    <div className="form-group my-3">
+                      <div className="col-auto">
+                        <div className="form-floating">
+                          <select
+                            className="form-select"
+                            id="industry"
+                            name="industry"
+                          >
+                            <option value="Admin/Business support">
+                              Admin/Business support
+                            </option>
+                            <option value="Agriculture, Forestry,Fishing and Hunting">
+                              Agriculture, Forestry,Fishing and Hunting
+                            </option>
+                            <option value="Arts, Entertainment and Recreation">
+                              Arts, Entertainment and Recreation
+                            </option>
+                            <option value="Constrution">Constrution</option>
+                            <option value="Education">Education</option>
+                            <option value="Finance and Insurance">
+                              Finance and Insurance
+                            </option>
+                            <option value="Healthcare and Social Assistance">
+                              Healthcare and Social Assistance
+                            </option>
+                            <option value="Hospitality">Hospitality</option>
+                            <option value="Information Technology">
+                              Information Technology
+                            </option>
+                            <option value="Manufacturing">Manufacturing</option>
+                            <option value="Mining and Mineral processing">
+                              Mining and Mineral processing
+                            </option>
+                            <option value="Professional, Scientific and Technical Services">
+                              Professional, Scientific and Technical Services
+                            </option>
+                            <option value="Real Estate">Real Estate</option>
+                            <option value="Retail">Retail</option>
+                            <option value="Transport and Logistics">
+                              Transport and Logistics
+                            </option>
+                            <option value="Other">Other</option>
+                          </select>
+                          <label htmlFor="industry" className="form-label">
+                            Business industry
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="form-group my-3">
+                      <div className="col-auto">
+                        <div className="form-floating">
+                          <select
+                            className="form-select"
+                            id="biz_phase"
+                            name="biz_phase"
+                          >
+                            <option value="phase_i">
+                              I have an idea but don’t know what to do next
+                            </option>
+                            <option value="phase_ii">
+                              I have a business but am not making money
+                            </option>
+                            <option value="phase_iii">
+                              I have products/services but I have poor sales
+                            </option>
+                            <option value="phase_iv">
+                              We are generating revenue, we would like to grow
+                              through investment
+                            </option>
+                            <option value="phase_v">
+                              I would like to be an entrepreneur but don’t know
+                              where to start
+                            </option>
+                          </select>
+                          <label htmlFor="biz_phase" className="form-label">
+                            Business phase
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="text-end">
+                      <button type="submit" className="btn bg-[#00c2cb] btn-info text-white">
+                        Save
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
