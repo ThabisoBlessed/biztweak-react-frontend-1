d8ab1c73faa26a4b937b3f997e7c5322a378d800
Ben Matela <bmatela@afrogeek.co.za>
2022-12-16 12:51:02 +0200

    init accordion

--- src/components/auth/BusinessAssessment.tsx
+++ src/components/auth/BusinessAssessment.tsx
@@ -2,7 +2,22 @@ import React from "react";
 
 export const BusinessAssessment = (props: any): JSX.Element => {
   const questionList: any[] = [
-    {id: "345342", name: "Channels", value: "Do you know how you are going to reach your customers?", answer: 0}
+    {
+      id: "345342",
+      name: "Channels",
+      questions: [
+        {
+          id: "dajsdflfsd",
+          value: "Do you know how you are going to reach your customers?",
+          answer: 0,
+        },
+        {
+          id: "drwtetwe",
+          value: "Post sales support has been provided.",
+          answer: 0,
+        },
+      ],
+    },
   ];
   return (
     <div>
@@ -27,10 +42,20 @@ export const BusinessAssessment = (props: any): JSX.Element => {
                   <b>Location:</b> n/a
                 </p>
                 <p>
-                  <b>Industry:</b> {props.industries.find((b: any) => b.id === props.businessIndustry).name}
+                  <b>Industry:</b>{" "}
+                  {
+                    props.industries.find(
+                      (b: any) => b.id === props.businessIndustry
+                    ).name
+                  }
                 </p>
                 <p>
-                  <b>Business phase:</b> {props.bizPhases.find((b: any) => b.id === props.businessPhase).name}
+                  <b>Business phase:</b>{" "}
+                  {
+                    props.bizPhases.find(
+                      (b: any) => b.id === props.businessPhase
+                    ).name
+                  }
                 </p>
               </div>
             </div>
@@ -38,53 +63,58 @@ export const BusinessAssessment = (props: any): JSX.Element => {
         </div>
 
         <div className="accordion" id="assessment-accordion">
-          <div className="accordion-item bg-[#f1feff]">
-            <h2 className="accordion-header" id="heading62">
-              <button
-                className="accordion-button collapsed text-dark"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapse62"
-                aria-expanded="false"
-                aria-controls="collapse62"
+          {questionList.map((question: any, index: number) => {
+            return (
+              <div
+                className="accordion-item bg-[#f1feff]"
+                key={index}
+                id={`${index}`}
               >
-                Channels
-              </button>
-            </h2>
+                <h2 className="accordion-header" id="heading62">
+                  <button
+                    className="accordion-button collapsed text-dark"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapse62"
+                    aria-expanded="false"
+                    aria-controls="collapse62"
+                  >
+                    {question.name}
+                  </button>
+                </h2>
+                <div
+                  id="collapse62"
+                  className="accordion-collapse collapse show"
+                  aria-labelledby="heading62"
+                  data-bs-parent="#assessment-accordion"
+                >
+                  <div className="accordion-body">
+                    <div className="question" id="yes_no_group_3">
+                      <p className="mb-1">{question.name}</p>
+                      <label htmlFor="question-yes-3">Yes</label>
+                      <input
+                        name="question-3"
+                        type="radio"
+                        value="1"
+                        id="question-yes-3"
+                        className="m-2"
+                      />
+                      <label htmlFor="question-no-3">No</label>
+                      <input
+                        name="question-3"
+                        type="radio"
+                        value="0"
+                        id="question-no-3"
+                        className="m-2"
+                      />
+                    </div>
 
-            <div
-              id="collapse62"
-              className="accordion-collapse collapse show"
-              aria-labelledby="heading62"
-              data-bs-parent="#assessment-accordion"
-            >
-              <div className="accordion-body">
-                <div className="question" id="yes_no_group_3">
-                  <p className="mb-1">
-                    Do you know how you are going to reach your customers?
-                  </p>
-                  <label htmlFor="question-yes-3">Yes</label>
-                  <input
-                    name="question-3"
-                    type="radio"
-                    value="1"
-                    id="question-yes-3"
-                    className="m-2"
-                  />
-                  <label htmlFor="question-no-3">No</label>
-                  <input
-                    name="question-3"
-                    type="radio"
-                    value="0"
-                    id="question-no-3"
-                    className="m-2"
-                  />
+                    <input name="assessment_id_4" type="hidden" value="4" />
+                  </div>
                 </div>
-
-                <input name="assessment_id_4" type="hidden" value="4" />
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
