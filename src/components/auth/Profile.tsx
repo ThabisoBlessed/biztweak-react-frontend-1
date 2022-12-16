8d6468b3281d9a5d1d38297fcd92c4a451c837ea
Ben Matela <bmatela@afrogeek.co.za>
2022-12-16 14:23:25 +0200

    profile fix

--- src/components/auth/Profile.tsx
+++ src/components/auth/Profile.tsx
@@ -134,12 +134,148 @@ export const Profile = () => {
           <ProfileMenu />
         </div>
         <div className="col-md-9 bg-white">
+          {showBizAssessment ? (
             <BusinessAssessment
               businessPhase={businessPhase}
               bizPhases={bizPhases}
               industries={industries}
               businessIndustry={businessIndustry}
             />
+          ) : (
+            <>
+              <div className="row justify-content-center">
+                <img
+                  src="https://biztweak.org.za/public/new/images/dashboard-placeholder.png"
+                  className="img-fluid col-md-5"
+                  alt=""
+                />
+              </div>
+
+              <div className="row justify-content-center">
+                <div className="col-md-6">
+                  <button
+                    type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#addCompany"
+                    className="btn btn-outline-info dotted-border col-12 mb-2 btn-lg"
+                  >
+                    <i className="fa fa-plus"></i> Add Company
+                  </button>
+                  <p className="text-center">
+                    After completing the assessment you will get a business
+                    health report that will give you recommendations on
+                  </p>
+                </div>
+              </div>
+
+              <div
+                className="modal fade"
+                id="addCompany"
+                tabIndex={-1}
+                aria-labelledby="addCompanyLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog modal-dialog-centered">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      >
+                        <i className="fa fa-close"></i>
+                      </button>
+                    </div>
+                    <div className="modal-body">
+                      <div className="text-center">
+                        <img
+                          src="https://biztweak.org.za/public/new/images/add-company.png"
+                          alt=""
+                          className="col-md-6 img-fluid"
+                        />
+                      </div>
+                      <h5>Add new company</h5>
+                      <form>
+                        <input type="hidden" />{" "}
+                        <div className="form-group my-3">
+                          <div className="col-auto">
+                            <div className="form-floating">
+                              <select
+                                className="form-select"
+                                id="industry"
+                                name="industry"
+                              >
+                                {industries.map(
+                                  (industry: any, index: number) => {
+                                    return (
+                                      <option
+                                        key={index}
+                                        id={`${index}`}
+                                        onClick={(e) =>
+                                          handleIndustryItemClick(e)
+                                        }
+                                        value={industry.value}
+                                      >
+                                        {industry.name}
+                                      </option>
+                                    );
+                                  }
+                                )}
+                              </select>
+                              <label htmlFor="industry" className="form-label">
+                                Business industry
+                              </label>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="form-group my-3">
+                          <div className="col-auto">
+                            <div className="form-floating">
+                              <select
+                                className="form-select"
+                                id="biz_phase"
+                                name="biz_phase"
+                              >
+                                {bizPhases.map((phase: any, index: number) => {
+                                  return (
+                                    <option
+                                      key={index}
+                                      id={`${index}`}
+                                      onClick={(e) =>
+                                        handleBizPhaseItemClick(e)
+                                      }
+                                      value={phase.value}
+                                    >
+                                      {phase.name}
+                                    </option>
+                                  );
+                                })}
+                              </select>
+                              <label htmlFor="biz_phase" className="form-label">
+                                Business phase
+                              </label>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="text-end">
+                          <button
+                            type="submit"
+                            className="btn bg-[#00c2cb] p-3 w-[100px]btn-info text-white"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                            onClick={(e) => onSave(e)}
+                          >
+                            Save
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
