2d781e50fc0c5340c3b1707c53adab3187397f50
Ben Matela <bmatela@afrogeek.co.za>
2022-12-16 18:24:03 +0200

    company profile modal

--- src/components/business/BusinessAssessment.tsx
+++ src/components/business/BusinessAssessment.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { CompleteCompanyProfileModal } from "./CompleteCompanyProfileModal";
 import { ManageBusiness } from "./ManageBusiness";
 
 export const BusinessAssessment = (props: any): JSX.Element => {
@@ -166,51 +167,7 @@ export const BusinessAssessment = (props: any): JSX.Element => {
           </button>
         </div>
 
-        <div
-          className="modal fade"
-          id="completeCompanyProfile"
-          tabIndex={-1}
-          aria-labelledby="completeCompanyProfileLabel"
-          aria-hidden="false"
-        >
-          <div className="modal-dialog modal-dialog-centered">
-            <div className="modal-content">
-              <div className="modal-header border-0">
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                ></button>
-              </div>
-              <div className="modal-body">
-                <div className="text-center">
-                  <img
-                    src="https://biztweak.org.za/public/new/images/complete-profile.png"
-                    alt=""
-                    className="col-md-6 img-fluid"
-                  />
-                  <h1 className="h3">Complete Company Profile</h1>
-                  <p>
-                    Completing your company information will help us know more
-                    about your business. Lets fill the assessment text:
-                    Completing your business assessment will help us know what
-                    kind of help your business needs.
-                  </p>
-                  <button
-                    data-bs-dismiss="modal"
-                    aria-label="Close"
-                    id="okBtn"
-                    type="button"
-                    className="btn bg-[#00c2cb] btn-info btn-lg mt-2 p-3 w-[100px] text-white"
-                    onClick={() => onSave()}
-                  >
-                    OK
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+          <CompleteCompanyProfileModal />
       </div>
     </div>
   );
---
+++ src/components/business/CompleteCompanyProfileModal.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+export const CompleteCompanyProfileModal = () => {
+  const onSave = () => {};
+
+  return (
+    <div>
+      <div
+        className="modal fade"
+        id="completeCompanyProfile"
+        tabIndex={-1}
+        aria-labelledby="completeCompanyProfileLabel"
+        aria-hidden="false"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header border-0">
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <div className="text-center">
+                <img
+                  src="https://biztweak.org.za/public/new/images/complete-profile.png"
+                  alt=""
+                  className="col-md-6 img-fluid"
+                />
+                <h1 className="h3">Complete Company Profile</h1>
+                <p>
+                  Completing your company information will help us know more
+                  about your business. Lets fill the assessment text: Completing
+                  your business assessment will help us know what kind of help
+                  your business needs.
+                </p>
+                <button
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                  id="okBtn"
+                  type="button"
+                  className="btn bg-[#00c2cb] btn-info btn-lg mt-2 p-3 w-[100px] text-white"
+                  onClick={() => onSave()}
+                >
+                  OK
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
