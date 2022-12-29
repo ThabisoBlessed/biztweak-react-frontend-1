5acc84a471184b28dc1c99e6e13e3242c0bd30ef
Ben Matela <bmatela@afrogeek.co.za>
2022-12-29 11:23:42 +0200

    before refactor

--- src/components/cmp/Answer.tsx
+++ src/components/cmp/Answer.tsx
@@ -1,28 +1,26 @@
 import React, { useState } from "react";
 
 export const Answer = () => {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
   const Input = () => {
     return (
-      <div className="answers">
-        <div className="d-flex align-items-center mb-2 answer">
-          <input
-            type="text"
-            className="form-control"
-            // placeholder={`Answer ${count}`}
-            placeholder={`Answer`}
-          />
-          <div className="form-check w-[100%] ms-2">
-            <label className="form-check-label">
-              <input
-                type="radio"
-                className="form-check-input"
-                name="answer"
-                autoComplete="off"
-              />
-              Correct Answer
-            </label>
-          </div>
+      <div className="d-flex align-items-center mb-2 answer" id={`${count}`}>
+        <input
+          type="text"
+          className="form-control"
+          // placeholder={`Answer ${count}`}
+          placeholder="Answer"
+        />
+        <div className="form-check w-[100%] ms-2">
+          <label className="form-check-label">
+            <input
+              type="radio"
+              className="form-check-input"
+              name="answer"
+              autoComplete="off"
+            />
+            Correct Answer
+          </label>
         </div>
       </div>
     );
@@ -42,7 +40,10 @@ export const Answer = () => {
           className="btn bg-[#6c757d] hover:bg-[#a1b0bc] text-white w-[150px]"
           type="button"
           id="add-answer"
-          onClick={() => { setCount(count + 1); onAddBtnClick() }}
+          onClick={() => {
+            onAddBtnClick();
+            setCount(count + 1);
+          }}
         >
           Add Answer
         </button>
