38eb56c3a04c33e35806704aa88e45c6c7ccc090
Ben Matela <bmatela@afrogeek.co.za>
2023-01-12 17:21:29 +0200

    cmp admin user

--- src/components/cmp/CMPAdminProfile.tsx
+++ src/components/cmp/CMPAdminProfile.tsx
@@ -1,20 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { getLocalStorageValue, LOCALSTORAGE_KEYS } from '../../config';
+import { IUser } from '../../model/user.model';
+import { getCurrentUser } from '../../services/lms/user.service';
 import { UserProfile } from '../shared/UserProfile'
 
 export const CMPAdminProfile = () => {
-  const navigate = useNavigate();
-  
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    window.history.pushState(null, "", document.URL);
-    window.addEventListener("popstate", function (event) {
-      navigate(-1);
-    });
-  }, []);
+    if (!user) {
+      getProfile();
+    }
+  });
+
+  const getProfile = async () => {
+    const storageUser = getLocalStorageValue(LOCALSTORAGE_KEYS.user);
+    if (storageUser) {
+      const userResult: IUser = JSON.parse(storageUser);
+      const profile = await getCurrentUser(userResult.id);
+      setUser(profile.data.package.data);
+      setIsLoading(false);
+
+      console.log(profile);
+    }
+  };
 
   return (
     <div>
-        <UserProfile menu={"cmp"} />
+      {isLoading ? <div className="text-center text-4xl h-[100vh]">Loading...</div> : (
+        <UserProfile menu={"cmp"} user={user} isLoading={isLoading} />
+      )}
     </div>
-  )
+  );
 }
