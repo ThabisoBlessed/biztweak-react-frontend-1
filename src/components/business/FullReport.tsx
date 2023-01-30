f7b5b04524ba3fa0539d7cdf54aa7379ff0b86e3
Ben Matela <bmatela@afrogeek.co.za>
2023-01-30 22:57:20 +0200

    init display

--- src/components/business/FullReport.tsx
+++ src/components/business/FullReport.tsx
@@ -35,15 +35,17 @@ export const FullReport = (props: any) => {
         percentage: number;
       }
     ) => {
-      if (!businessSoFar[category]) businessSoFar[category] = [];
-      businessSoFar[category].push({
-        questionId,
-        answer,
-        output,
-        category,
-        type,
-        percentage: 0
-      });
+      if (output.length > 0) {
+        if (!businessSoFar[category]) businessSoFar[category] = [];
+        businessSoFar[category].push({
+          questionId,
+          answer,
+          output,
+          category,
+          type,
+          percentage: 0,
+        });
+      }
       return businessSoFar;
     },
     {}
@@ -70,15 +72,17 @@ export const FullReport = (props: any) => {
         percentage: number;
       }
     ) => {
-      if (!businessSoFar[category]) businessSoFar[category] = [];
-      businessSoFar[category].push({
-        questionId,
-        answer,
-        output,
-        category,
-        type,
-        percentage: 0
-      });
+      if (output.length > 0) {
+        if (!businessSoFar[category]) businessSoFar[category] = [];
+        businessSoFar[category].push({
+          questionId,
+          answer,
+          output,
+          category,
+          type,
+          percentage: 0,
+        });
+      }
       return businessSoFar;
     },
     {}
@@ -91,7 +95,7 @@ export const FullReport = (props: any) => {
   useEffect(() => {
     if (!isLoggedIn()) navigate("/auth/login");
     // console.log(fullReport);
-    
+
     // setDisplayBusinessStructure(businessStructureDisplay);
 
     console.log(displayBusinessStructure);
@@ -124,63 +128,72 @@ export const FullReport = (props: any) => {
               <div id="collapse1" className="accordion-collapse collapse show">
                 <div className="accordion-body">
                   <p className="text-dark">Business Diagnosis</p>
-                  <div id="accordion1" className="accordion">
-                    <div className="accordion-item bg-transparent cust-accordion">
-                      <h2 className="accordion-header" id="headingOne">
-                        <button
-                          className="accordion-button collapsed bg-[#f1feff]"
-                          type="button"
-                          data-bs-toggle="collapse"
-                          data-bs-target="#collapse2"
-                          aria-expanded="false"
-                          aria-controls="collapseOne"
-                          data-bs-parent="#accordion1"
-                        >
-                          Priority Elements
-                        </button>
-                        <div id="collapse2" className="accordion-body">
-                          <ul className="list-group">
-                            <li className="list-group-item bg-[#f1feff]">
-                              <p className="text-2xl">
-                                Value proposition
-                                <span className="badge float-end rounded-pill bg-[#00c2cb]">
-                                  30%
-                                </span>
-                              </p>
-                              <div className="clearfix"></div>
-                              <ul className="list-group list-group-numbered">
-                                <div className="list-group-item d-flex justify-content-between align-items-start list-group-item-danger">
-                                  <p className="mb-0 color-code text-sm">
-                                    Problem being solved not determined
-                                  </p>
-                                </div>
-                                <div className="list-group-item d-flex justify-content-between align-items-start list-group-item-danger">
-                                  <p className="mb-0 color-code text-sm">
-                                    Value being delivered not determined.
-                                  </p>
-                                </div>
-                                <div className="list-group-item d-flex justify-content-between align-items-start list-group-item-success">
-                                  <p className="mb-0 color-code text-sm">
-                                    Customer needs have been determined.
-                                  </p>
-                                </div>
-                                <div className="list-group-item d-flex justify-content-between align-items-start list-group-item-danger">
-                                  <p className="mb-0 color-code text-sm">
-                                    Unique selling point not determined.
-                                  </p>
-                                </div>
-                                <div className="list-group-item d-flex justify-content-between align-items-start list-group-item-danger">
-                                  <p className="mb-0 color-code text-sm">
-                                    No elevator pitch
-                                  </p>
-                                </div>
-                              </ul>
-                            </li>
-                          </ul>
-                        </div>
-                      </h2>
-                    </div>
-                  </div>
+                  {isLoading ? null : (
+                    <>
+                      {displayBusinessConcept.map(
+                        (concept: any, index: number) => {
+                          return (
+                            <div
+                              id={`accordion${index}`}
+                              className="accordion"
+                              key={index}
+                            >
+                              <div className="accordion-item bg-transparent cust-accordion">
+                                <h2
+                                  className="accordion-header"
+                                  id="headingOne"
+                                >
+                                  <button
+                                    className="accordion-button collapsed bg-[#f1feff]"
+                                    type="button"
+                                    data-bs-toggle="collapse"
+                                    data-bs-target={`#structure_collapse${index}`}
+                                    aria-expanded="false"
+                                    aria-controls="collapseOne"
+                                    data-bs-parent={`accordion${index}`}
+                                  >
+                                    {concept[0]}
+                                  </button>
+                                  <div
+                                    id={`structure_collapse${index}`}
+                                    className="accordion-body"
+                                  >
+                                    <ul className="list-group">
+                                      <li className="list-group-item bg-[#f1feff]">
+                                        <p className="text-sm">
+                                          {concept[0]}
+                                          <span className="badge float-end rounded-pill bg-[#00c2cb]">
+                                            {concept[1].percentage}%
+                                          </span>
+                                        </p>
+                                        <div className="clearfix"></div>
+                                        <ul className="list-group list-group-numbered">
+                                          {concept[1].map(
+                                            (response: any, i: number) => {
+                                              return (
+                                                <div
+                                                  key={i}
+                                                  className="list-group-item d-flex justify-content-between align-items-start list-group-item-danger"
+                                                >
+                                                  <p className="mb-0 color-code text-sm">
+                                                    {response.output}
+                                                  </p>
+                                                </div>
+                                              );
+                                            }
+                                          )}
+                                        </ul>
+                                      </li>
+                                    </ul>
+                                  </div>
+                                </h2>
+                              </div>
+                            </div>
+                          );
+                        }
+                      )}
+                    </>
+                  )}
                 </div>
               </div>
             </div>
@@ -239,7 +252,7 @@ export const FullReport = (props: any) => {
                                   >
                                     <ul className="list-group">
                                       <li className="list-group-item bg-[#f1feff]">
-                                        <p className="text-2xl">
+                                        <p className="text-sm">
                                           {structure[0]}
                                           <span className="badge float-end rounded-pill bg-[#00c2cb]">
                                             {structure[1].percentage}%
@@ -247,12 +260,20 @@ export const FullReport = (props: any) => {
                                         </p>
                                         <div className="clearfix"></div>
                                         <ul className="list-group list-group-numbered">
-                                          <div className="list-group-item d-flex justify-content-between align-items-start list-group-item-danger">
-                                            <p className="mb-0 color-code text-sm">
-                                              Problem being solved not
-                                              determined
-                                            </p>
-                                          </div>
+                                          {structure[1].map(
+                                            (response: any, i: number) => {
+                                              return (
+                                                <div
+                                                  key={i}
+                                                  className="list-group-item d-flex justify-content-between align-items-start list-group-item-danger"
+                                                >
+                                                  <p className="mb-0 color-code text-sm">
+                                                    {response.output}
+                                                  </p>
+                                                </div>
+                                              );
+                                            }
+                                          )}
                                         </ul>
                                       </li>
                                     </ul>
